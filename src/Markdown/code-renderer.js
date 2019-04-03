c69810cd7bfa33bed58f2d2d088a108b4c8ee4df
Marwane Boudriga <mboudriga@Marwane-Boudrigas-MacBook-Pro.local>
2019-04-03 09:54:15 -0400

    fix component

--- src/Markdown/code-renderer.js
+++ src/Markdown/code-renderer.js
@@ -6,27 +6,24 @@ import js from 'highlight.js/lib/languages/javascript';
 
 Lowlight.registerLanguage('js', js);
 
-const CodeBlock = React.createClass({
-    displayName: 'CodeBlock',
-    propTypes: {
+export default class CodeBlock extends React.Component {
+    static propTypes = {
         literal: PropTypes.string,
         language: PropTypes.string,
         inline: PropTypes.bool
-    },
+    };
 
     shouldComponentUpdate(nextProps, nextState) {
         return shallowCompare(this, nextProps, nextState);
-    },
-
-    render: function() {
+    }
+  
+    render() {
         return (
             <Lowlight
-                language={this.props.language || 'js'}
-                value={this.props.literal}
-                inline={this.props.inline}
-            />
+                    language={this.props.language || 'js'}
+                    value={this.props.literal}
+                    inline={this.props.inline}
+                />
         );
     }
-});
-
-export default CodeBlock;
+  }
